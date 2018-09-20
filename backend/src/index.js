1fd6d60690813c24c8036dbe007157d2abe16886
eddy0 <awindy369@gmail.com>
2018-09-20 01:37:34 -0700

    config with graphql yoga

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,14 @@
-// lets go!
+const log = console.log.bind(console)
+require('dotenv').config({path: 'variables.env'})
+const createServer = require('./createServer')
+const db = require('./db')
+const server = createServer()
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    }
+}, address => {
+    log(`server is running on port http://localhost:${address.port}`)
+})
