fdea29ef67f858deeee5e61e3e90c7aab5f80d42
eddy0 <awindy369@gmail.com>
2018-09-20 20:27:34 -0700

    prisma and yoga

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
-const Mutations = {};
+const Mutations = {
+    createDog(parent, args, ctx, info) {
+        console.log('args',parent, args)
+    },
+}
 
-module.exports = Mutations;
+module.exports = Mutations
